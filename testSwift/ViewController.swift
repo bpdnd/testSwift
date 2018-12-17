91e37111fdb36cc782ac881714ee0d4054b08e2a
bpdnd <3438291751@qq.comgit config --global user.email 3438291751@qq.com>
2018-12-17 08:34:59 +0800

    初始化

---
+++ testSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testSwift
+//
+//  Created by Admin on 2018/12/17.
+//  Copyright © 2018年 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.red;
+    }
+
+
+}
+
